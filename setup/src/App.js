64b2d97aaa51a59c3cb86b159b42a6404bcb545a
Muhammad Asif <asif88383@gmail.com>
2022-04-11 11:59:59 +0500

    first commit

--- setup/src/App.js
+++ setup/src/App.js
@@ -1,8 +1,31 @@
 import React, { useState, useEffect } from 'react'
 import { useFetch } from './useFetch'
 import Follower from './Follower'
+
 function App() {
-  return <h2>pagination starter</h2>
+  const [page, setPage] = useState(0)
+  const [followers, setFollowers] = useState([])
+  const { loading, data } = useFetch()
+
+  useEffect(() => {
+    if(loading){
+      return setFollowers(data[page])
+    }
+  }, [loading, page])
+
+  const nextPage = () => {
+    setPage((oldPage) => {
+      let nextPage = oldPage + 1
+      if(nextPage > data.length - 1){
+        nextPage = 0
+      }
+      return nextPage
+    })
+  }
+
+  return (
+
+  )
 }
 
 export default App
