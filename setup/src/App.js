bbf9548f48f1fd59e39a8cdd3a1d182a95fde75f
Muhammad Asif <asif88383@gmail.com>
2022-04-11 12:05:21 +0500

    first commit

--- setup/src/App.js
+++ setup/src/App.js
@@ -23,8 +23,55 @@ function App() {
     })
   }
 
-  return (
+  const prevPage = () => {
+    setPage((oldPage) => {
+      let prevPage = oldPage - 1
+      if(prevPage < 0){
+        prevPage = data.length - 1
+      }
+      return prevPage
+    })
+  }
 
+  const handlePage = (index) => {
+    setPage(index)
+  }
+
+  return (
+    <main>
+      <div className='section-title'>
+        <h1>{loading ? 'loading...' : 'pagination'}</h1>
+        <div className='underline'></div>
+      </div>
+      <section className='followers'>
+        <div className='container'>
+          {followers.map((follower) => {
+            return <Follower key={follower.id} {...follower} />
+          })}
+        </div>
+        {!loading && (
+          <div className='btn-container'>
+            <button className='prev-btn' onClick={prevPage}>
+              prev
+            </button>
+            {data.map((item, index) => {
+              return (
+                <button
+                  key={index}
+                  className={`page-btn ${index === page ? 'active-btn' : null}`}
+                  onClick={() => handlePage(index)}
+                >
+                  {index + 1}
+                </button>
+              )
+            })}
+            <button className='next-btn' onClick={nextPage}>
+              next
+            </button>
+          </div>
+        )}
+      </section>
+    </main>
   )
 }
 
